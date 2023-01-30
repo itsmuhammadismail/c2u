4fecf6054e1d2fbc7daff8e8a2a17e869fd5eb66
Muhammad Ismail <itsmuhammadismail@gmail.com>
2023-01-30 10:14:18 +0500

    ios

--- lib/app.dart
+++ lib/app.dart
@@ -14,10 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
 
     return MultiRepositoryProvider(
       providers: RepositoryProviders.providers,
